def44a1d6bc079cb5c6a788b8bbb403dc66738e4
fldesamais <Florian.Desamais@etu.udamail.fr>
2018-06-06 15:19:48 +0200

    Modif libellé LogOut en Deconnexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'LogOut', component: LoginPage }
+      { title: 'Déconnexion', component: LoginPage }
     ];
 
   }
